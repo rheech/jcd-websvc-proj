4c3514e5a9385160de7f86b21f09c4562cce3d30
Cheonghyun Lee <rheech@users.noreply.github.com>
2015-11-19 07:39:01 +0000

    Create DB IO outline

--- 01_ServiceProducer/libdbio/OrderManager.cs
+++ 01_ServiceProducer/libdbio/OrderManager.cs
@@ -5,6 +5,31 @@ using System.Text;
 
 namespace libdbio
 {
+    // We never work on rainy days, and national holidays!! ==> Web Service
+
+    public struct CustomerInfo
+    {
+        public string CustomerID;
+        public string CompanyName;
+        public string Address;
+        public string PhoneNumber;
+    }
+
+    public struct OrderInfo
+    {
+        public string OrderID;
+        public string OrderStatus;
+        public CustomerInfo Customer;
+        public string Requirement;
+    }
+
+    public struct Product
+    {
+        public string ProductID;
+        public string ProductName;
+        public double Price;
+    }
+
     public class OrderManager : DatabaseIO
     {
         public OrderManager(string dbFileName) : base()
@@ -12,9 +37,30 @@ namespace libdbio
 
         }
 
-        public void CreateOrder()
+        public void CreateCustomer(CustomerInfo info)
         {
+        }
 
+        public void FindCustomer(CustomerInfo info)
+        {
+        }
+
+        public void CustomerExists(CustomerInfo info)
+        {
+        }
+
+        public void FindProduct(Product product)
+        {
+        }
+
+        public void CreateOrder(CustomerInfo info, Product product)
+        {
+            // Return Order info as an invoice
+            // Send email for order confirmation
+        }
+
+        public void RetrieveOrder(OrderInfo info)
+        {
         }
     }
 }
